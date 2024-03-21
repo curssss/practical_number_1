e6415f34efa2c18da7f02da1e63b5f59efb59339
user <user@DESKTOP-J9PIPDE>
2024-03-21 15:27:13 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic;
+
+namespace practical_number_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logical logical = new Logical();
+            logical.AddStudent("j", "f", "f");
+            logical.AddStudent("jf", "ff", "ff");
+            Console.WriteLine(logical.GetAll());
+            Console.ReadKey();
+        }
+    }
+}
